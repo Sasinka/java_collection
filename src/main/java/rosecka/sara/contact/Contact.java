0b463a82da7a9a4dd693bf673b2e2f67c024ffc6
rosec <roseckasara@gmail.com>
2022-04-08 19:42:02 +0200

    init comit

---
+++ src/main/java/rosecka/sara/contact/Contact.java
@@ -0,0 +1,10 @@
+package rosecka.sara.contact;
+
+public class Contact {
+    private String firstName;
+    private String surname;
+    private String phoneNumber;
+    private String address;
+
+
+}
